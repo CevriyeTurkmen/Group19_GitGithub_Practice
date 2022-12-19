63a3f8cf375ee5ec8a5216c8fcb63726b1612fce
Koray <koray_4404@hotmail.com>
2022-12-19 23:23:35 +0000

    Updated class Koray

---
+++ src/test/java/Koray.java
@@ -0,0 +1,11 @@
+public class Koray {
+
+    public static void main(String[] args) {
+        System.out.println("Hello my friends");
+
+        System.out.println("Good luck with the new project");
+
+
+    }
+
+}
