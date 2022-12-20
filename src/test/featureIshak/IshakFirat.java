245abb0254ae04ed44c05c8f9e265a314a057307
Ishak Firat <iishakfirat@gmail.com>
2022-12-20 21:34:33 +0000

    created IshakFirat class

---
+++ src/test/featureIshak/IshakFirat.java
@@ -0,0 +1,17 @@
+package src.test.featureIshak;
+
+public class IshakFirat {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello friends");
+
+
+
+    }
+
+
+
+
+
+}
